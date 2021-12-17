c9e4af8290a591312094d46b6b32eb564b231196
Ewa Kubik <ewa.kubik@hotmail.com>
2021-12-17 22:29:31 +1100

    modified html/js - activated countdown timer

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(codeQuizQuestions[1].question);
+var timeLeft;
+var countdown;
+var startButton;
+var counter;
+
+function startQuiz() {
+  timeLeft = 75;
+
+  document.getElementById("secondsLeft").innerText = timeLeft;
+
+  countdown = setInterval(function () {
+    timeLeft--;
+    document.getElementById("secondsLeft").innerText = timeLeft;
+    if (timeLeft === 0) {
+      clearInterval(countdown);
+    }
+  }, 1000);
+}
+
+startButton = document.querySelector("quiz-start-btn")
