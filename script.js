b2a4053b61713aa8296751de981e4705ea0e60d5
Ewa Kubik <ewa.kubik@hotmail.com>
2021-12-18 19:07:20 +1100

    edited js

--- script.js
+++ script.js
@@ -9,26 +9,25 @@ var options;
 var wrongAnswerChosen = document.getElementById("incorrect-answer");
 var correctAnswerChosen = document.getElementById("correct-answer");
 var hideElement = document.querySelectorAll(".hide");
-var returnStart = document.getElementById("backToStart");
+//var returnStart = document.getElementById("backToStart");
 var clearScore = document.getElementById("clear-score");
 var quizBody = document.getElementById("quiz-body");
 var endScreen = document.getElementById("game-over");
+var questionsContainer = document.getElementById("quiz-questions-container");
+var questionElement = document.getElementById("question-text");
 
-var optionOne = document.getElementById("option1")
-var optionTwo = document.getElementById("option2");
-var optionThree = document.getElementById("option3");
-var optionFour = document.getElementById("option4");
-
-returnStart.onclick = function () {
-  window.location.href = "./index.html";
-};
-
+//returnStart.onclick = function () {
+  //window.location.href = "./index.html";
+//};
+//
 function startQuiz() {
   // hide quiz intro and start button //
   for (var i = 0; i < hideElement.length; i++) {
     hideElement[i].style.display = "none";
   }
   //display first question//
+
+  //questionsContainer.style.display = "block";
   displayQuestions();
 
   //function starts timer, replaces inner text of span with time remaining//
@@ -47,21 +46,49 @@ function startQuiz() {
   }, 1000);
 }
 
+
+
 function displayQuestions() {
   var yourQuestion = codeQuizQuestions[qIndex];
   var questionsText = document.getElementById("question-text");
   questionsText.textContent = yourQuestion.question;
 
-
-
-  //for (var i = 0; i < codeQuizQuestions.options.length; i++) {
-   // function optionsLoop() {
-    //  var choiceButton = document.createElement("button");
-    //  choiceButton.textContent = codeQuizQuestions.options[i];
-     // answerChoices.append(choiceButton);
-  //  }
- // }
+  for (var i = 0; i < codeQuizQuestions[qIndex].options.length; i++) {
+  var choiceButton = document.createElement("button");
+  choiceButton.textContent = codeQuizQuestions[qIndex].options[i];
+  answerChoices.append(choiceButton);
+   }
+  // }
 }
+
+//function displayQuestions() {
+ // showQuestion(codeQuizQuestions[questionIndex]);
+//}
+
+//function showQuestion() {
+  //questionElement.innerText = codeQuizQuestions[questionIndex].question;
+//}
+
+//var yourQuestion = codeQuizQuestions[questionIndex];
+//var questionsText = document.getElementById("question-text");
+//questionsText.textContent = yourQuestion.question;
+
+//for (var i = 0; i < codeQuizQuestions.options.length; i++) {
+// function optionsLoop() {
+//  var choiceButton = document.createElement("button");
+//  choiceButton.textContent = codeQuizQuestions.options[i];
+// answerChoices.append(choiceButton);
+//  }
+// }
+
+/*function optionsLoop() {
+  for (var i = 0; i < codeQuizQuestions[qIndex].options.length; i++) {
+    let buttons = document.createElement("button");
+    buttons.textContent = codeQuizQuestions[qIndex].options[i];
+    answerChoices.append(buttons);
+  }
+}
+optionsLoop();
 
 function correctAnswer() {
   correctAnswerChosen.style.display = "block";
@@ -70,4 +97,4 @@ function correctAnswer() {
 function wrongAnswer() {
   wrongAnswerChosen.style.display = "block";
   timeLeft = timeLeft - 10;
-}
+}*/
